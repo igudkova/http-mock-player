a8bbbbb2de72ea7065b130bb81b5d0c47723ee04
Irina Gudkova <irinagudkova@gmail.com>
2016-07-03 14:09:59 +0200

    Updated reference to HttpMockPlayer library in the Samples project.

--- HttpMockPlayer/Properties/AssemblyInfo.cs
+++ HttpMockPlayer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HttpMockPlayer")]
-[assembly: AssemblyCopyright("Copyright © 2016 Irina Gudkova")]
+[assembly: AssemblyCopyright("Copyright © Irina Gudkova 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
